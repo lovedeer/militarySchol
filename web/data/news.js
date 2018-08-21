b4b0acd9c8b55e29b551f79638080b294836b8c4
zhoulong <zhoulong1588@163.com>
2018-08-22 01:43:41 +0800

    init

---
+++ web/data/news.js
@@ -0,0 +1,311 @@
+var news = [{
+    menu: "网络学校",
+    display: true,
+    board: [
+        {
+            name: "政治理论",
+            display: true,
+            article: [
+                {
+                    title: "习近平向俄罗斯当选总统普京致贺电",
+                    display: true,
+                    img: "img/img3.jpg",
+                    url: "article1.html",
+                    style: "font-weight:600",
+                    carousel: true
+                },
+                {
+                    title: "李克强主持召开国务院常务会议",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+
+
+        }
+        , {
+            name: "军事高科技",
+            display: true,
+            article: [
+                {
+                    title: "胡塞武装击落F-15 不经意间创造两个“第一”",
+                    display: true,
+                    img: "img/img1.jpg",
+                    url: "article1.html",
+                    carousel: true
+                },
+                {
+                    title: "中国某新型潜舰导弹系统获国家科技进步特等奖",
+                    display: true,
+                    img: "img/img2.jpg",
+                    url: "article1.html",
+                    carousel: true
+                }
+            ]
+        },
+        {
+            name: "学法用法",
+            display: true,
+            article: [
+                {
+                    title: "江苏启动环保专项行动督查",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "土耳其货币崩盘！中国金融体系能抗风险吗？",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+        }
+    ]
+}, {
+    menu: "政治课堂",
+    display: true,
+    board: [
+        {
+            name: "指导员课件",
+            display: true,
+            article: [
+                {
+                    title: "汪洋会见法国国民议会第一副议长博纳尔",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "王沪宁出席纪念马克思诞辰理论研讨会",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+
+
+        }
+        , {
+            name: "支队专题教育",
+            display: true,
+            article: [
+                {
+                    title: "认证花钱就能买？有机食品市场乱象调查",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "江西为敢于担当的干部“撑腰” 六种情况可容错免责",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+        },
+        {
+            name: "地方教授课件",
+            display: true,
+            article: [
+                {
+                    title: "江西为敢于担当的干部“撑腰” 六种情况可容错免责",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "陕西一厅官被降为科员退休待遇 纪委:长期不交党费",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+        }
+    ]
+}, {
+    menu: "网上军史馆",
+    display: true,
+    board: [
+        {
+            name: "兵种军史",
+            display: true,
+            article: [
+                {
+                    title: "独家|美国制裁逼急伊朗 中东笼罩核战争阴云？",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "没对比就没伤害：S400与爱国者3谁更牛？",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+
+
+        }
+        , {
+            name: "学院军史",
+            display: true,
+            article: [
+                {
+                    title: "十九大精神学习",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "军事强国",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+        },
+        {
+            name: "中队概况",
+            display: true,
+            article: [
+                {
+                    title: "十九大精神学习",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "军事强国",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+        }
+    ]
+}, {
+    menu: "娱乐在线",
+    display: true,
+    board: [
+        {
+            name: "励志歌曲",
+            display: true,
+            article: [
+                {
+                    title: "十九大精神学习",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "军事强国",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+
+
+        }
+        , {
+            name: "军事电影",
+            display: true,
+            article: [
+                {
+                    title: "十九大精神学习",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "军事强国",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+        },
+        {
+            name: "军事题材游戏",
+            display: true,
+            article: [
+                {
+                    title: "十九大精神学习",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: true
+                },
+                {
+                    title: "军事强国",
+                    display: true,
+                    img: "",
+                    url: "",
+                    carousel: false
+                }
+            ]
+        }
+    ]
+}, {
+    menu: "电子报刊",
+    display: true,
+    article: [
+        {
+            title: "对手主帅“吐槽”中国女排！郎平板脸表情包又上线",
+            display: true,
+            img: "",
+            url: ""
+        },
+        {
+            title: "亚运第二日：中国继续领跑金牌榜 孙杨徐嘉余再添金",
+            display: true,
+            img: "",
+            url: ""
+        },
+        {
+            title: "亚运第二日：中国继续领跑金牌榜 孙杨徐嘉余再添金",
+            display: true,
+            img: "",
+            url: ""
+        },
+        {
+            title: "亚运第二日：中国继续领跑金牌榜 孙杨徐嘉余再添金",
+            display: true,
+            img: "",
+            url: ""
+        },
+        {
+            title: "亚运第二日：中国继续领跑金牌榜 孙杨徐嘉余再添金",
+            display: true,
+            img: "",
+            url: ""
+        },
+        {
+            title: "亚运第二日：中国继续领跑金牌榜 孙杨徐嘉余再添金",
+            display: true,
+            img: "",
+            url: ""
+        }
+    ]
+}];
---
+++ web/js/index.js
@@ -0,0 +1,254 @@
+var $articleList = $(".articleList"), $articleDetail = $(".activeDetail"), $indexDiv = $(".indexDiv"),
+    $menuLi;
+
+$(document).ready(function () {
+    var $ul = $(".msNav");
+    news.forEach(function (obj) {
+        var html = [];
+        if (obj.menu && obj.display) {
+            if (hasShowBoard(obj)) {
+                html.push(" <li class='dropdown'>" +
+                    "<a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true'" +
+                    " aria-expanded='false' data-menu='" + obj.menu + "'>" + obj.menu + "<span class='caret'></span></a><ul class='dropdown-menu'>");
+                obj.board.forEach(function (board) {
+                    if (board.display) {
+                        html.push("<li><a href='#' data-menu='" + obj.menu + "' data-type='board' data-page='" + board.name + "'>" + board.name + "</a></li><li role='separator' class='divider'></li>")
+                    }
+                });
+                html.push("</ul></li>");
+            } else {
+                html.push("<li><a href='#' data-menu='" + obj.menu + "' data-page='" + obj.menu + "' data-type='noBoard'>" + obj.menu + "</a></li>");
+            }
+            $ul.append(html.join(''));
+        }
+    });
+    $menuLi = $ul.find('li');
+
+    $("#indexPage").click(function () {
+        $indexDiv.show();
+        $articleDetail.children().remove();
+        $articleDetail.text('');
+        $articleList.children().remove();
+        $menuLi.removeClass('active');
+        $(this).parent().addClass('active');
+    });
+
+    genCarousel();
+    genBoardDiv();
+    regImgClick();
+    regMenuClick();
+});
+
+/**
+ * 生成首页图片轮播
+ */
+function genCarousel() {
+    var $carouselInner = $(".carousel-inner");
+    news.forEach(function (obj) {
+        if (obj.menu && obj.display) {
+            if (hasShowBoard(obj)) {
+                obj.board.forEach(function (board) {
+                    if (board.display && board.article && board.article.length > 0) {
+                        board.article.forEach(function (article) {
+                            if (article.display && article.carousel && article.img && article.img.length > 0 && article.url && article.url.length > 0) {
+                                $carouselInner.append("<div class='item'>" +
+                                    "<img src='" + article.img + "'>" +
+                                    "<div class='carouselDiv'><a href='#' data-menu='" + obj.menu + "' class='carouselTitle' data-type='article' data-page='" + article.url + "'>" + article.title + "</a></div></div>")
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    });
+    var item = $carouselInner.find(".item");
+    if (item.length === 0) {
+        $(".carousel").parent().remove();
+    } else {
+        $(item[0]).addClass('active');
+    }
+
+    $(".carousel").append("<a class='left carousel-control' href='#myCarousel' role='button' data-slide='prev'>\n" +
+        "                <span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>\n" +
+        "                <span class='sr-only'>Previous</span>\n" +
+        "            </a>\n" +
+        "            <a class='right carousel-control' href='#myCarousel' role='button' data-slide='next'>\n" +
+        "                <span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>\n" +
+        "                <span class='sr-only'>Next</span>\n" +
+        "            </a>")
+}
+
+/**
+ * 生成首页各模块文章列表
+ */
+function genBoardDiv() {
+    var num = 1;
+    news.forEach(function (obj) {
+        var html = [];
+        if (obj.display) {
+            html.push("<div class='boardDiv " + ((num % 2) ? 'odd' : '') + "'><div class='bdTitle'><a class='boardAll' href='#' data-menu='" + obj.menu + "'>" + obj.menu + "</a></div>");
+            html.push("<div><ul>");
+            var count = 0;
+            if (obj.board && obj.board.length > 0) {
+                obj.board.forEach(function (board) {
+                    if (board.display && count < 6) {
+                        if (board.article && board.article.length > 0) {
+                            board.article.forEach(function (article) {
+                                if (article.display && article.title && count < 6) {
+                                    count++;
+                                    html.push("<li><a href='#' style='" + article.style + "' data-menu='" + obj.menu + "' data-url='" + article.url + "'>" + article.title + "</a></li>")
+                                }
+                            })
+                        }
+                    }
+                })
+            } else if (obj.article && obj.article.length > 0) {
+                obj.article.forEach(function (article) {
+                    if (article.display && article.title && count < 6) {
+                        count++;
+                        html.push("<li><a href='#' style='" + article.style + "' data-menu='" + obj.menu + "' data-url='" + article.url + "'>" + article.title + "</a></li>")
+                    }
+                })
+            }
+            if (count === 0) {
+                return;
+            }
+            html.push("</ul></div></div>");
+            $indexDiv.append(html.join(''));
+            num++;
+        }
+    })
+}
+
+
+/**
+ * 注册菜单栏点击事件
+ */
+function regMenuClick() {
+    $menuLi.find('a').click(function () {
+        if ($(this).text() === '首页') {
+            return;
+        }
+        var dataPage = $(this).attr('data-page');
+        if (!dataPage) {
+            return;
+        }
+        var article = [];
+        var dateType = $(this).attr('data-type'), dataMenu = $(this).attr('data-menu');
+        if (dateType === 'board') {
+            article = findArticleByBoardName(dataPage);
+        }
+        else if (dateType === 'noBoard') {
+            article = findArticleByMenuName(dataPage);
+        }
+
+        genArticleList(article, dataMenu);
+        $menuLi.removeClass('active');
+        $(".msNav a[data-menu=" + dataMenu + "]").parent().addClass('active');
+        $indexDiv.hide();
+    })
+}
+
+/**
+ * 生成文章列表
+ * @param article
+ */
+function genArticleList(article, dataMenu) {
+    var html = [];
+    if (article && article.length > 0) {
+        html.push("<ul class='articleListUl'>");
+        article.forEach(function (article) {
+            if (article.display) {
+                html.push(" <li><a href='#' data-menu='" + dataMenu + "' style='" + article.style + "' data-page='" + article.url + "'>" + article.title + "</a></li>")
+            }
+        });
+        html.push("</ul>")
+    }
+    $indexDiv.hide();
+    $articleList.children().remove();
+    $articleDetail.children().remove();
+    $articleList.append(html.join(''));
+}
+
+/**
+ * 根据版块名查找文章列表
+ * @param boardName
+ */
+function findArticleByBoardName(boardName) {
+    var articles = [], find = false;
+    news.every(function (obj) {
+        obj.board.every(function (board) {
+            if (board.name === boardName) {
+                articles = board.article;
+                find = true;
+                return false;
+            }
+            return true;
+        });
+        return !find;
+    });
+    return articles;
+}
+
+/**
+ * 没有版块的菜单根据菜单名查找文章列表
+ * @param menuName
+ */
+function findArticleByMenuName(menuName) {
+    var articles = [];
+    news.every(function (obj) {
+        if (obj.menu === menuName) {
+            articles = obj.article;
+            return false;
+        }
+        return true;
+    });
+    return articles;
+}
+
+/**
+ * 注册首页图片及文字链接点击事件
+ */
+function regImgClick() {
+    $(".carousel-inner img").click(function () {
+        var a = $(this).next().find('a');
+        articleLinkClick(a);
+    });
+
+    $(".carousel-inner .carouselDiv a").click(function () {
+        articleLinkClick(this);
+    })
+}
+
+/**
+ * 文章链接点击事件
+ */
+function articleLinkClick(a) {
+    var url = $(a).attr('data-page'),
+        menu = $(a).attr('data-menu');
+    $.ajax({
+        url: 'article/' + url,
+        type: 'get',
+        success: function (data) {
+            $menuLi.removeClass('active');
+            $(".msNav a[data-menu=" + menu + "]").parent().addClass('active');
+            $indexDiv.hide();
+            $articleDetail.append(data);
+        }
+    })
+}
+
+function hasShowBoard(obj) {
+    if (!obj.board || obj.board.length === 0) {
+        return false;
+    }
+    var has = false;
+    obj.board.every(function (board) {
+        if (board.display) {
+            has = true;
+            return false;
+        }
+        return true;
+    });
+    return has;
+}
